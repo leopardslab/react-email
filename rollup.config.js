9f200dcc12da1920a410ee27337885f4a44983fb
Janvi <janvi@summachar.in>
2022-02-11 13:02:48 +0530

    Added react, basic structure, jest files and test component

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,30 @@
-import { defineConfig } from 'rollup';
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
 
-const rollupConfig = defineConfig({
-  input: 'src/trial.ts',
-  output: [
-    {
-      file: 'dist/bundle.cjs.js',
-      format: 'cjs',
-    },
-  ],
-});
+const packageJson = require("./package.json");
 
-export default rollupConfig;
+export default {
+    input: "src/index.ts",
+    output: [
+        {
+            file: packageJson.main,
+            format: "cjs",
+            sourcemap: true
+        },
+        {
+            file: packageJson.module,
+            format: "esm",
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        peerDepsExternal(),
+        resolve(),
+        commonjs(),
+        typescript({ useTsconfigDeclarationDir: true }),
+        postcss()
+    ]
+};
---
+++ src/TestComponent/TestComponent.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { TestComponentProps } from "./TestComponent.types";
+
+import "./TestComponent.scss";
+
+const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
+    <div
+        data-testid="test-component"
+        className={`test-component test-component-${theme}`}
+    >
+        <h1 className="heading">I'm the test component</h1>
+    </div>
+);
+
+export default TestComponent;
