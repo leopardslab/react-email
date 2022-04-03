891c5fcd436a52432267d4b4de8379b1236c7562
Devesh <rockdev1234560@gmail.com>
2022-04-03 23:34:05 +0530

    Init for React + TS + Rollup

---
+++ src/components/Test.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Test.css';
+
+class Test extends React.Component {
+  render () {
+    return (
+      <div className="test">Test</div>
+    );
+  }
+}
+
+export default Test;
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import Test from './components/Test.tsx';
+export default Test;
