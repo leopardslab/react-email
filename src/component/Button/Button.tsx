5e353567165317e3820cf8ad8941b4f2aa8df078
wishwashan <ravindu.18@cse.mrt.ac.lk>
2022-04-04 12:26:00 +0530

    add rollup to react-email

---
+++ src/component/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+
+import "./Button.css";
+import { ButtonProps } from "./Button.types";
+
+const Button: FC<ButtonProps> = ({ type, text, onClick }) => {
+  return (
+    <button type="button" className={`Button Button-${type}`} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
