15f2f354f9efa0785298b50152e12c7da99729d3
Niloy Sikdar <niloysikdar30@gmail.com>
2022-06-28 16:04:31 +0530

    fix(generateemail): fix generateEmail
    fix generateEmail replace method for styles and HTML body
    
    Signed-off-by: Niloy Sikdar <niloysikdar30@gmail.com>

--- src/utils/generateEmail/generateEmail.ts
+++ src/utils/generateEmail/generateEmail.ts
@@ -5,12 +5,17 @@ import { deafultHTML } from './defaultHTML';
 interface GenerateEmailOptions {
   baseTemplate?: string;
   baseStyles?: string;
+  baseStylesReplacementString?: string;
+  baseBodyReplacementString?: string;
 }
 
 export const generateEmail = (
   jsxElement: React.ReactElement,
   options?: GenerateEmailOptions,
 ): string => {
+  const baseStylesReplacementString = options?.baseStylesReplacementString || 'EMAIL_BASE_STYLES';
+  const baseBodyReplacementString = options?.baseBodyReplacementString || 'EMAIL_BODY_CONTENT';
+
   const baseHTML: string = options?.baseTemplate
     ? fs.readFileSync(options.baseTemplate, 'utf8').toString()
     : deafultHTML;
@@ -21,11 +26,8 @@ export const generateEmail = (
 
   const JSXtoHTML = ReactDOMServer.renderToStaticMarkup(jsxElement);
 
-  const styleEndPos = baseHTML.indexOf('</style>');
-  const styledHTML = baseHTML.slice(0, styleEndPos) + '\n' + baseCSS + baseHTML.slice(styleEndPos);
-
-  const bodyEndPos = styledHTML.indexOf('</body>');
-  const finalHTML = styledHTML.slice(0, bodyEndPos) + JSXtoHTML + styledHTML.slice(bodyEndPos);
+  const styledHTML = baseHTML.replace(baseStylesReplacementString, baseCSS);
+  const finalHTML = styledHTML.replace(baseBodyReplacementString, JSXtoHTML);
 
   return finalHTML;
 };
