71390e796dfdd5efe2e713819d74fab60095dd03
Niloy Sikdar <niloysikdar30@gmail.com>
2022-07-05 20:13:27 +0530

    feat(component): add Link component (#51)
    * feat(component): add link component
    
    add a new Link component to add links to email
    
    resolves #43
    
    Signed-off-by: Niloy Sikdar <niloysikdar30@gmail.com>
    
    * docs(storybook): link stories
    
    add stories for Link component
    
    resolves #43
    
    Signed-off-by: Niloy Sikdar <niloysikdar30@gmail.com>
    
    * fix(component): fix Link component
    
    fix Link component with the updated structure and props
    
    resolves #43
    
    Signed-off-by: Niloy Sikdar <niloysikdar30@gmail.com>

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,1 +1,1 @@
-export { Button } from './Button';
+export { Button, ButtonProps } from './Button';
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './Email';
 export * from './Section';
 export * from './Column';
 export * from './Button';
+export * from './Link';
