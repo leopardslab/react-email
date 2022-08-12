449755b5aaedc8b2e41024376a4bfbdc757312cc
Niloy Sikdar <niloysikdar30@gmail.com>
2022-08-13 02:23:18 +0530

    docs(jsdoc): add comment for BaseStyleProp
    Add JSDoc comments for BaseStyleProp interface
    
    re #61
    
    Signed-off-by: Niloy Sikdar <niloysikdar30@gmail.com>

--- src/components/types/index.ts
+++ src/components/types/index.ts
@@ -1,6 +1,12 @@
 import { CSSProperties } from 'react';
 
 export interface BaseStyleProp<T extends string> {
+  /**
+   * The classes object to be merged with the base styles.
+   */
   classes?: Partial<Record<T, CSSProperties>>;
+  /**
+   * The class name to be applied to the element.
+   */
   className?: string;
 }
