2491ebfd093dba561dfe7c62b78e906f6d8af753
Niloy Sikdar <niloysikdar30@gmail.com>
2022-08-13 02:00:54 +0530

    docs(jsdoc): add comments for generateTextEmailFromHTML
    Add JSDoc comments for genearteTextEmailFromHTML utility
    
    Resolves #61
    
    Signed-off-by: Niloy Sikdar <niloysikdar30@gmail.com>

--- src/utils/generateTextEmailFromHTML/generateTextEmailFromHTML.ts
+++ src/utils/generateTextEmailFromHTML/generateTextEmailFromHTML.ts
@@ -1,5 +1,11 @@
 import htmlToPlainText from 'textversionjs';
 
+/**
+ * Function to convert HTML Email to Text version.
+ * @param {string}  html - HTML which needs to be converted.
+ * @returns {string} Text version of the email in string format.
+ */
+
 export const generateTextEmailFromHTML = (html: string): string => {
   const plainText = htmlToPlainText(html, { headingStyle: 'hashify' });
   return plainText;
