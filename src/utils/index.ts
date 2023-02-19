7c4ea637259702a4bb6f22a5f7091847a6cdac0c
Niloy Sikdar <niloysikdar30@gmail.com>
2023-02-19 21:32:39 +0530

    perf(validateemail): remove nodemailer and its related dependency
    Remove nodemailer and whole validateEmail method from the package and add them to the docs
    
    fix #133
    
    Signed-off-by: Niloy Sikdar <niloysikdar30@gmail.com>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,5 +4,3 @@ export type { CSSClasses } from './makeStyles';
 export { generateEmail } from './generateEmail';
 export { generateTextEmailFromHTML } from './generateTextEmailFromHTML';
 export { generateTextEmail } from './generateTextEmail';
-export { validateEmail } from './validateEmail';
-export type { MailtrapOptions } from './validateEmail';
